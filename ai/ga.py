f0364fd7ea49f20212be94723eeed450599cb568
ia7ck <ik0390@gmail.com>
2018-11-03 16:21:47 +0900

    rename

--- ai/ga.py
+++ ai/ga.py
@@ -1,11 +1,11 @@
-import dataclasses, random, operator
+import dataclasses, random, operator, copy
 from typing import List
 from ai.cost_func_ai import CostFuncAi
 from game import Board, pieces, SCORES
 
 
 @dataclasses.dataclass
-class Genetic:
+class Gene:
     coefficients: List[int]
     score: int
 
@@ -24,15 +24,15 @@ def calc_fitness(ai: CostFuncAi, board: Board) -> int:
     return score
 
 
-def genetic_algorithm(population_size: int = 10, gen_limit: int = 10) -> List[int]:
+def genetic_algorithm(population_size: int = 12, gen_limit: int = 10) -> List[int]:
     ai = CostFuncAi()
     board = Board()
-    population: List[Genetic] = []
+    population: List[Gene] = []
     # init
-    for i in range(population_size):
+    for _ in range(population_size):
         coeffs = [random.randint(-100, 100) for _ in range(3)]
         ai.coefficients = coeffs
-        population.append(Genetic(coeffs, calc_fitness(ai, board)))
+        population.append(Gene(coeffs, calc_fitness(ai, board)))
     # selection, genetic operation
     for _ in range(gen_limit):
         population.sort(key=operator.attrgetter("score"), reverse=True)
@@ -40,10 +40,10 @@ def genetic_algorithm(population_size: int = 10, gen_limit: int = 10) -> List[in
     # termination
     best_score = -1
     best_coeffs: List[int] = []
-    for g in population:
-        if g.score > best_score:
-            best_score = g.score
-            best_coeffs = g.coefficients
+    for individual in population:
+        if individual.score > best_score:
+            best_score = individual.score
+            best_coeffs = individual.coefficients
     print("best score : {}".format(best_score))
     print("best coeffs : {}".format(best_coeffs))
     return best_coeffs
