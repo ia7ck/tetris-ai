f37dd753de61df7adccb00e6fe168f4d0d0ac9b4
ia7ck <ik0390@gmail.com>
2018-11-03 16:00:43 +0900

    fix calc_fitness

--- ai/ga.py
+++ ai/ga.py
@@ -1,6 +1,7 @@
-import dataclasses, random
+import dataclasses, random, operator
 from typing import List
 from ai.cost_func_ai import CostFuncAi
+from game import Board, pieces, SCORES
 
 
 @dataclasses.dataclass
@@ -9,21 +10,33 @@ class Genetic:
     score: int
 
 
-def calc_fitness(ai: CostFuncAi) -> int:
-    return 0
+def calc_fitness(ai: CostFuncAi, board: Board) -> int:
+    board.clear()
+    score = 0
+    while True:
+        given_piece_set = random.choice(pieces)
+        action = ai.get_action(board, given_piece_set)
+        can_put = board.proceed(action)
+        if not can_put:
+            break
+        rm_line_num = board.resolve()
+        score += SCORES[rm_line_num]
+    return score
 
 
-def genetic_algorithm(population_size: int = 100, gen_limit: int = 10) -> List[int]:
+def genetic_algorithm(population_size: int = 10, gen_limit: int = 10) -> List[int]:
     ai = CostFuncAi()
+    board = Board()
     population: List[Genetic] = []
     # init
     for i in range(population_size):
         coeffs = [random.randint(-100, 100) for _ in range(3)]
         ai.coefficients = coeffs
-        population.append(Genetic(coeffs, calc_fitness(ai)))
+        population.append(Genetic(coeffs, calc_fitness(ai, board)))
     # selection, genetic operation
     for _ in range(gen_limit):
-        pass
+        population.sort(key=operator.attrgetter("score"), reverse=True)
+
     # termination
     best_score = -1
     best_coeffs: List[int] = []
