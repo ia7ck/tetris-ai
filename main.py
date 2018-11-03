dac69a085784808bd7ca00803695278bc32bb4d6
ia7ck <ik0390@gmail.com>
2018-11-03 15:32:59 +0900

    fix log

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys, random
 import tkinter as tk
 from typing import List
 import game
 import graphic
+import ai.monte_carlo
+import ai.cost_func_ai
+import ai.ga
 
 
 class Tetris:
@@ -13,31 +17,32 @@ class Tetris:
         self.score = 0
 
     def play(self):
-        import random
+        print("[start]")
+        self.run()
 
+    def run(self):
         given_piece_set = random.choice(self.pieces)
         action = self.ai.get_action(self.board, given_piece_set)
         can_put = self.board.proceed(action)
         self.gui.field.draw(self.board)
         if not can_put:
-            print("can't put the piece with x = {}:".format(action.x0))
-            print(action.piece)
-            print("score : {}".format(self.score))
+            self.tear_down(action)
             return
         wait_time = 100
         rm_line_num = self.board.resolve()
         self.score += game.SCORES[rm_line_num]
         if rm_line_num > 0:
             wait_time += 200
-        self.gui.after(wait_time, self.play)
+        self.gui.after(wait_time, self.run)
+
+    def tear_down(self, action: game.Action):
+        print("[end]")
+        print("can't put the piece with x = {}:".format(action.x0))
+        print(action.piece)
+        print("score : {}".format(self.score))
 
 
 def main():
-    import sys
-    import ai.monte_carlo
-    import ai.cost_func_ai
-    import ai.ga
-
     tetris = Tetris()
     tetris.ai = ai.cost_func_ai.CostFuncAi(coefficients=ai.ga.genetic_algorithm())
 
